4384bb4f53601c694c5018056a8f342e50b2c60d
Rasmus Jensen <rasmusdj96@gmail.com>
2023-01-06 09:33:29 +0100

    removed scss files

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,4 +1,3 @@
-// import './App.scss';
 import { Header } from "./components/partials/Header/Header";
 import { Nav } from "./components/partials/Nav/Nav";
 import { Footer } from "./components/partials/Footer/Footer";
