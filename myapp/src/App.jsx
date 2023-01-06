75be2b6b7706de237767e4af916e34dd38b6c937
Rasmus Jensen <rasmusdj96@gmail.com>
2023-01-06 09:49:53 +0100

    imported sass

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/partials/Header/Header";
 import { Nav } from "./components/partials/Nav/Nav";
 import { Footer } from "./components/partials/Footer/Footer";
 import { Main } from "./components/partials/Main/Main";
+import "/app.scss";
 
 function App() {
 	return (
