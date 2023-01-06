031153cae28c3a021c4080a9d7745da8d625d273
Rasmus Jensen <rasmusdj96@gmail.com>
2023-01-06 09:20:38 +0100

    123

--- myapp/src/components/partials/Nav/Nav.jsx
+++ myapp/src/components/partials/Nav/Nav.jsx
@@ -1,19 +1,25 @@
 const navArr = [
-    "Forside",
-    "Produkter",
-    "Om os",
-    "Ledige stillinger",
-    "Kontakt os"
-]
+	"Forside",
+	"Produkter",
+	"Om os",
+	"Ledige stillinger",
+	"Kontakt os",
+];
 
 export const Nav = () => {
-    return (
-        <nav>
-            {navArr.map((nav, key) => {
-                return (
-                    <a key={key} href={"/" + nav}>{nav}</a>
-                )
-            })}
-        </nav>
-    )
-}
+	return (
+		<nav>
+			<ul>
+				{navArr.map((nav, key) => {
+					return (
+						<li>
+							<a key={key} href={"/" + nav}>
+								{nav}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+};
